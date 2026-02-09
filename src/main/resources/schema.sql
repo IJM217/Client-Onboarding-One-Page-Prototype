2fee1f65f0cb2bfbea1fe14b8055a28797ebb0bc
joelmasters217@gmail.com <joelmasters217@gmail.com>
2026-02-09 17:19:47 +0200

    Update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,12 @@
-CREATE TABLE IF NOT EXISTS clients(
-    Full_Name VARCHAR(255) NOT NULL,
-    DOB DATE NOT NULL,
+CREATE TABLE IF NOT EXISTS clients (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    dob DATE NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(50) NOT NULL,
-    Nationality VARCHAR(100) NOT NULL,
-    ID VARCHAR(13) PRIMARY KEY,
-    Passport VARCHAR(50) PRIMARY kEY,
-    BankStatement BLOB,
-    File_Name VARCHAR(255)
-)
+    nationality VARCHAR(100) NOT NULL,
+    id_number VARCHAR(13),
+    passport_number VARCHAR(50),
+    bank_statement BLOB,
+    file_name VARCHAR(255)
+);
